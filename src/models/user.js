4a600dbe6d768732956da26a13da16090c2e8eb5
Akoz <likingvirus1@gmail.com>
2023-12-04 10:38:44 -0700

    user

---
+++ src/models/user.js
@@ -0,0 +1,11 @@
+// import { z } from 'zod'
+
+// export const User = z.object({
+//   id: z.number({
+//     coerce: true,
+//     required_error: 'User ID is required'
+//   }),
+//   name: z.string(),
+//   email: z.string().email(),
+//   password: z.string()
+// })
