84928de6a322177286989f2a152734d6dfbb18fb
Zemogadrian <124641782+Zemogadrian@users.noreply.github.com>
2023-12-15 13:27:58 -0700

    cambios

--- src/app/pages/login.js
+++ src/app/pages/login.js
@@ -1,8 +1,12 @@
+import { Image } from 'expo-image'
 import React, { useState } from 'react'
-import { StyleSheet, View, TextInput, Alert, Text, TouchableOpacity } from 'react-native'
+import { StyleSheet, View, TextInput, Text } from 'react-native'
+import Ok from '../../../assets/ok'
+import No from '../../../assets/no'
+import Logo from '../../../assets/logo.png'
 
 export default function Login () {
-  const [email, setEmail] = useState('')
+  const [user, setUser] = useState('')
   const [password, setPassword] = useState('')
 
   return (
@@ -12,9 +16,7 @@ export default function Login () {
           width: '35%',
           borderWidth: 1
         }}
-      >
-        <Text>Imagen</Text>
-      </View>
+      />
       <View
         style={{
           flex: 1,
@@ -23,35 +25,42 @@ export default function Login () {
           gap: 5
         }}
       >
-        <TextInput
-          style={styles.input}
-          placeholder='Correo electrónico'
-          onChangeText={setEmail}
-          value={email}
-          textContentType='emailAddress'
-        />
-        <TextInput
-          style={styles.input}
-          placeholder='Contraseña'
-          secureTextEntry
-          onChangeText={setPassword}
-          value={password}
-          textContentType='password'
-        />
-        <TouchableOpacity
-          onPress={() => {
-            if (email === '' || password === '') {
-              Alert.alert('Por favor, introduce tu correo electrónico y contraseña')
-            } else {
-              Alert.alert(`Bienvenido ${email}`)
-            }
+        <Image source={Logo} style={styles.image} contentFit='cover' />
+        <View style={styles.inputBox}>
+          <TextInput
+            style={styles.input}
+            placeholder='USUARIO'
+            onChangeText={setUser}
+            value={user}
+            textContentType='username'
+          />
+        </View>
+        <View style={styles.inputBox}>
+          <TextInput
+            style={styles.input}
+            placeholder='CONTRASEÑA'
+            secureTextEntry
+            onChangeText={setPassword}
+            value={password}
+            textContentType='password'
+          />
+        </View>
+
+        <Text style={{ color: 'gray' }}>CARGANDO...</Text>
+        <View style={{ flexDirection: 'row', gap: 10 }}>
+          <Ok style={{
+            width: '100%',
+            height: '100%'
           }}
-        >
-          <Text>
-            Iniciar sesión
-          </Text>
-        </TouchableOpacity>
+          />
+          <No style={{
+            width: '100%',
+            height: '100%'
+          }}
+          />
+        </View>
       </View>
+
     </View>
   )
 }
@@ -61,13 +70,35 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row'
   },
-  input: {
+  image: {
+    width: '70%',
+    height: '35%',
+    paddingVertical: 10
+
+  },
+  image2: {
+    width: '100%',
+    height: '100%',
+    paddingVertical: 10
+
+  },
+  inputBox: {
+    width: 350,
     height: 40,
-    width: 150,
-    borderColor: 'gray',
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    paddingHorizontal: 16,
+    fontSize: 16,
+    color: '#000000',
+    marginVertical: 10,
     borderWidth: 1,
-    paddingLeft: 8,
-    paddingRight: 8,
-    borderRadius: 5
+    shadowRadius: 5,
+    shadowOpacity: 1,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 0 },
+    elevation: 10,
+    textAlign: 'center',
+    justifyContent: 'center'
   }
+
 })
