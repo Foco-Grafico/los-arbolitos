24f764885734511cbb551f8ad3e96b04b8e826ff
Zemogadrian <124641782+Zemogadrian@users.noreply.github.com>
2023-12-27 10:51:04 -0700

    cambios

---
+++ src/app/pages/kitchen.js
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+
+export default function Kitchen () {
+  const mesero = 'Juan'
+  return (
+    <View>
+      <Text>MESERO {mesero}</Text>
+    </View>
+  )
+}
