808fe3ad5eb1bf2359c91492f135a9397e5d5edb
Akoz <likingvirus1@gmail.com>
2024-01-10 13:30:23 -0700

    Add dependencies and implement offline notification

--- App.js
+++ App.js
@@ -1,25 +1,42 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Route } from './src/app/components/route'
-import Home from './src/app/home'
 import Login from './src/app/pages/login'
 import Cashier from './src/app/pages/cashier'
 import Admin from './src/app/pages/admin'
 import Kitchen from './src/app/pages/kitchen'
 import { SetScreenOrientation } from './src/lib/orientation'
-import { LogBox, View } from 'react-native'
+import { LogBox, Text, View, Modal } from 'react-native'
 import { Waiter } from './src/app/pages/waiter'
 import { routes } from './src/lib/data'
 import { StatusBar } from 'expo-status-bar'
 import Empleados from './src/app/pages/admin/empleados'
+import NetInfo from '@react-native-community/netinfo'
+import * as Notifications from 'expo-notifications'
 
 LogBox.ignoreLogs(['new NativeEventEmitter', 'Aborted'])
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false
+  })
+})
+
 export default function App () {
+  const [isConnected, setIsConnected] = useState(true)
+
   useEffect(() => {
     SetScreenOrientation()
       .catch((err) => {
         console.log(err)
       })
+
+    const unsuscribe = NetInfo.addEventListener(state => {
+      setIsConnected(state.isConnected)
+    })
+
+    return () => unsuscribe()
   }, [])
 
   return (
@@ -29,9 +46,33 @@ export default function App () {
       }}
     >
       <StatusBar hidden />
-      <Route name='home'>
-        <Home />
-      </Route>
+      <Modal
+        visible={!isConnected}
+        animationType='fade'
+        transparent
+        statusBarTranslucent
+      >
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'rgba(0,0,0,0.5)'
+          }}
+        >
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 20,
+              color: 'white',
+              backgroundColor: 'red',
+              padding: 10
+            }}
+          >
+            No hay conexión a internet
+          </Text>
+        </View>
+      </Modal>
       <Route name='login'>
         <Login />
       </Route>
