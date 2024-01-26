30b7abe0670162d2ca08b385100d136096c28297
KikeDeAlba <likingvirus1@gmail.com>
2024-01-25 22:47:21 -0700

    Update finishOrderInCashier function to include payment type modal

--- src/app/func/finish-order-in.cashier.js
+++ src/app/func/finish-order-in.cashier.js
@@ -1,6 +1,6 @@
 import { APIENDPOINTS } from '../../lib/api-call/data'
 
-export default function finishOrderInCashier (orderId, discount) {
+export default function finishOrderInCashier (orderId, discount, type) {
   const headers = new Headers()
   headers.append('accept', 'application/json')
 
@@ -9,5 +9,5 @@ export default function finishOrderInCashier (orderId, discount) {
     headers
   }
 
-  return fetch(`${APIENDPOINTS.finishOrder}?order_id=${orderId}&discount=${discount}`, options)
+  return fetch(`${APIENDPOINTS.finishOrder}?order_id=${orderId}&discount=${discount}&type=${type}`, options)
 }
