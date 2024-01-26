82eb4e18addc4db20bc4b078eb7a3d156dac87a1
Zemogadrian <124641782+Zemogadrian@users.noreply.github.com>
2024-01-25 19:52:33 -0700

    Fix formatting and styling issues in cashier and kitchen components

--- src/app/components/kitchen/actual-dish.js
+++ src/app/components/kitchen/actual-dish.js
@@ -129,6 +129,7 @@ export default function ActualDish ({ setOrders, bar = false }) {
                 <Text
                   style={{
                     fontWeight: 'bold'
+
                   }}
                 >
                   CANTIDAD
@@ -156,14 +157,14 @@ export default function ActualDish ({ setOrders, bar = false }) {
                 </Text>
                 {dish.supplies_modified[item].map((supply) => (
                   <View key={v4()}>
-                    <Text>
+                    <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
                       {supply.name} {supply.quantity}
                     </Text>
                   </View>
                 ))}
               </View>
               <Text style={{ color: '#005943', fontWeight: 'bold', fontSize: 15 }}>OBSERVACIONES</Text>
-              <Text>{(dish?.comments[index] !== 'null' && dish?.comments[index] !== 'undefined') && dish?.comments[index]}</Text>
+              <Text style={{ fontSize: 24, fontWeight: 'bold' }}>{(dish?.comments[index] !== 'null' && dish?.comments[index] !== 'undefined') && dish?.comments[index]}</Text>
             </View>
           )}
         />
