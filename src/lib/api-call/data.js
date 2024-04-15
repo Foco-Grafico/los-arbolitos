6759a82d8c416e6127e9ad4052c5871184ef9fb6
KikeDeAlba <likingvirus1@gmail.com>
2024-04-15 08:03:56 -0700

    Update app version to 3.1.7 and API_URL in data.js to use new ngrok URL

--- src/lib/api-call/data.js
+++ src/lib/api-call/data.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://ecfe-74-208-129-103.ngrok-free.app'
+export const API_URL = 'https://arbolitos.ngrok.app'
 // export const API_URL = 'http://192.168.1.194:3001'
 
 export const APIENDPOINTS = {
